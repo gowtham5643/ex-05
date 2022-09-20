06dfc2375ea22523aaa5936d5349086696251505
gowtham5643 <111501086+gowtham5643@users.noreply.github.com>
2022-09-20 23:35:42 +0530

    Delete pro1/pro2 directory

--- pro1/pro2/pack1.java
+++
@@ -1,9 +0,0 @@
-package main.sub1;
-
-public class pack1 {
-
-	    public void show1()
-	    {
-	        System.out.println("Sub pack 1");
-	    }
-}
--- pro1/pro2/pack2.java
+++
@@ -1,8 +0,0 @@
-package main.sub2;
-
-public class pack2 {
-	 public void show2()
-	    {
-	        System.out.println("sub pack 2");
-	    }
-}
